5937f6365bd501ce48d2376b9c8059e7b92d19f0
RUGVEDLANGHI17 <89995278+RUGVEDLANGHI17@users.noreply.github.com>
2021-11-20 00:03:37 +0530

    homepage controller added

---
+++ hotel_management/src/main/java/com/example/hotel_management/homepageController.java
@@ -0,0 +1,121 @@
+package com.example.hotel_management;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class homepageController {
+
+    @FXML
+    private AnchorPane AP;
+
+    @FXML
+    private Rectangle head;
+
+    @FXML
+    private Text destiny;
+
+    @FXML
+    private Rectangle db;
+
+    @FXML
+    private MenuButton menu;
+
+    @FXML
+    private Line dl1;
+
+    @FXML
+    private Line dl2;
+
+    @FXML
+    private ImageView main_img;
+
+    @FXML
+    private Text moto;
+
+    @FXML
+    private Text dt;
+
+    @FXML
+    private TextField dtb;
+
+    @FXML
+    private Text ci;
+
+    @FXML
+    private TextField cib;
+
+    @FXML
+    private Text co;
+
+    @FXML
+    private TextField cob;
+
+    @FXML
+    private Button sb;
+
+    @FXML
+    private Rectangle foot;
+
+    @FXML
+    private Text hp;
+
+    @FXML
+    private Text tac;
+
+    @FXML
+    private Text pn;
+
+    @FXML
+    private Text ct;
+
+    @FXML
+    private Circle c1;
+
+    @FXML
+    private TextField t1;
+
+    @FXML
+    private Circle c2;
+
+    @FXML
+    private Circle c3;
+
+    @FXML
+    private Circle c4;
+
+    @FXML
+    private Circle c5;
+
+    @FXML
+    private Circle c6;
+
+    @FXML
+    private Circle c7;
+
+    @FXML
+    private TextField t2;
+
+    @FXML
+    private TextField t3;
+
+    @FXML
+    private TextField t4;
+
+    @FXML
+    private TextField t5;
+
+    @FXML
+    private TextField t6;
+
+    @FXML
+    private TextField t7;
+
+}
