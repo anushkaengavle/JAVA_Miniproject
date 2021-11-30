8afa2878804639ce031fec6322004c4b53402c62
RUGVEDLANGHI17 <89995278+RUGVEDLANGHI17@users.noreply.github.com>
2021-11-30 20:42:48 +0530

    controller sign up page

--- hotel_management/src/main/java/com/example/hotel_management/signuppageController.java
+++ hotel_management/src/main/java/com/example/hotel_management/signuppageController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class signuppageController {
@@ -13,13 +14,26 @@ public class signuppageController {
     private Button NB2;
 
     @FXML
-    void signup(ActionEvent event) {
+    private TextField TF1;
 
-    }
+    @FXML
+    private TextField TF2;
+
+    @FXML
+    private TextField TF3;
+
+    @FXML
+    private TextField TF4;
+
+    @FXML
+    private TextField TF5;
+
+    @FXML
+    private TextField TF6;
+
     @FXML
     public void ntbutton2 (ActionEvent event) throws Exception{
-
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 750);
         Stage stage = new Stage();
         stage.setTitle("Hello!");
